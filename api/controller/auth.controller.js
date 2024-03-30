ef4e3f6f5a49a4a0ccf5146a6cf222d95326d7d1
arun1118 <arunpanigrahi18@gmail.com>
2024-03-30 23:15:59 +0530

    add user from sign up page UI

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -2,20 +2,12 @@ import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs'
 
 export const signup = async (req,res,next)=>{
-
-    let newUser=undefined
-    try{        
-        const {username, email, password} = req.body;
-        const hashPassword=bcryptjs.hashSync(password, 8)
-        newUser = new User({username, email, password: hashPassword})
-    }
-    catch(error) {
-        console.log("error creating user")
-        res.status(500).json(error.messaage)
-    }
-
+    const {username, email, password} = req.body;
+    const hashPassword=bcryptjs.hashSync(password, 8)
+    const newUser = new User({username, email, password: hashPassword})
     try{
        await newUser.save()
+       console.log("successfully saved a new user")
        res.status(201).json({message: "user created and saved successfully"}) 
     }
     catch(error){
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    try{
+      setLoading(true)
+      setError(false)
+      const res = await fetch("/api/auth/signup",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+      const resData = await res.json()
+      setLoading(false)
+      if(resData.success === false){
+        setError(true)
+        return;
+      }
+    }
+    catch(error) {
+      setLoading(false)
+      setError(true)
+      console.log("err",error)
+    }
+    setFormData({})
+  }
+
   return (
-    <div>SignUp</div>
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" id="username" onChange={handleChange} />
+        <input type="email" placeholder="email" id="email" onChange={handleChange} />
+        <input type="password" placeholder="password" id="password" onChange={handleChange} />
+        <button> {loading ? "Loading" : "Sign Up"} </button>
+      </form>
+      <Link to="/signin"><p>sign in?</p></Link>
+      <p>{error && "Something went wrong!!"}</p>
+    </div>
   )
 }
 
