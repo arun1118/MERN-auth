d565665ed19970a5076af8bc18c6995c2d18a624
arun1118 <arunpanigrahi18@gmail.com>
2024-03-31 01:00:29 +0530

    complete signin functionality, include token in cookie and login user from UI

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res,next)=>{
     const {username, email, password} = req.body;
@@ -12,6 +14,22 @@ export const signup = async (req,res,next)=>{
     }
     catch(error){
         console.log("error saving a new user")
+        next(error)
+    }
+}
+
+export const signin = async (req,res,next)=>{
+    const {username, password} = req.body;
+    try{
+        const validUser = await User.findOne({username})
+        if(!validUser) return next(errorHandler(401,"User not found"))
+        const isMatchPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!isMatchPassword) return next(errorHandler(404, "Invalid credentials!!"))
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_CODE)
+        const {password: hashedpassword, ...userData} = validUser._doc
+        const expiryDate = new Date(Date.now()+3600000)
+        res.cookie('access_token', token, {httpOnly: true, expires: expiryDate}).status(200).json(userData)
+    }catch (error){
         next(error)
     }
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    try{
+      setLoading(true)
+      setError(false)
+      const res = await fetch("/api/auth/signin",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+      const resData = await res.json()
+      setLoading(false)
+      if(resData.success === false){
+        setError(true)
+        return;
+      }
+      navigate("/")
+    }
+    catch(error) {
+      setLoading(false)
+      setError(true)
+      console.log("err",error)
+    }
+    setFormData({})
+  }
+
   return (
-    <div>SignIn</div>
+    <div>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" id="username" onChange={handleChange} />
+        <input type="password" placeholder="password" id="password" onChange={handleChange} />
+        <button> {loading ? "Loading" : "Sign In"} </button>
+      </form>
+      <Link to="/signup"><p>sign up?</p></Link>
+      <p>{error && "Something went wrong!!"}</p>
+    </div>
   )
 }
 
