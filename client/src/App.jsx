fe1e9549999e3b6be417963201cf942d266124ff
arun1118 <arunpanigrahi18@gmail.com>
2024-03-30 16:58:45 +0530

    create vite react app, create routes and basic pages

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Profile from './pages/Profile.jsx';
+import Help from './pages/Help.jsx';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/help" element={<Help />}/>
+      <Route path="/signin" element={<SignIn />}/>
+      <Route path="/signup" element={<SignUp />}/>
+      <Route path="/profile" element={<Profile />}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
